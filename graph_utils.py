cf8ff211be27ad2db05058c2f100476f6784fbdd
Ubuntu <ubuntu@ip-172-31-21-26.us-west-2.compute.internal>
2018-05-05 12:44:44 +0000

    Parallelized graph evaluation

--- graph_utils.py
+++ graph_utils.py
@@ -3,10 +3,12 @@ import os
 import argparse
 import itertools
 import numpy as np
+import math
 
 from datetime import datetime
 from threading import RLock
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Process
+from multiprocessing import Queue as MPQueue
 
 class Graph:
 
@@ -73,7 +75,8 @@ class Node:
         self.connection_ids.add(other_node.get_id())
 
     def connected_to_id(self, node_id):
-        return node_id in self.connections
+        result = (node_id in self.connection_ids)
+        return result
 
     def connected_to_node(self, node):
         return self.connected_to_node(node.get_id())
@@ -137,61 +140,103 @@ def method_pref_attachment(node_1, node_2):
 def create_scorer(method_fn, correctness_threshold):
     return lambda node_1, node_2 : method_fn(node_1, node_2) >= correctness_threshold
 
-def eval_link_prediction_method(graph, scorer_fn):
+def eval_link_prediction_method(graph, scorer_fn, num_workers=20):
     node_ids = list(graph.get_node_ids())
 
-    # Concurrent read/write operations are 
-    # not executed on the same dictionary entry,
-    # and, due to the enforced determinism and symmetry
-    # of `scorer_fn`, the results dictionary can
-    # therefore always be modified in lock-free fashion
-    results = {}
+    results_queue = MPQueue()
 
-    def eval_helper(idx_low, idx_high):
-        correct = 0
-        explored = 0
-        for i in xrange(idx_low, idx_high):
-            i_node_id = node_ids[i]
-            i_node = graph.get_node(i_node_id)
+    def eval_helper(graph, node_ids, r1_low, r1_high, r2_low, r2_high, results_queue):
+        try:
+            print(r1_low, r1_high, r2_low, r2_high)
+            results = set()
+            correct_negative = 0
+            correct_positive = 0
+            total_negative = 0
+            total_positive = 0
+            for i in xrange(r1_low, r1_high):
+                i_node_id = node_ids[i]
+                i_node = graph.get_node(i_node_id)
 
-            for j_node_id in node_ids:
-                results_key = frozenset((i_node_id, j_node_id))
-                if results_key in results or i_node_id == j_node_id:
-                    continue
+                for j in xrange(r2_low, r2_high):
+                    j_node_id = node_ids[j]
+                    results_key = frozenset((i_node_id, j_node_id))
+                    if results_key in results or i_node_id == j_node_id:
+                        continue
 
-                j_node = graph.get_node(j_node_id)
-                explored += 1
+                    j_node = graph.get_node(j_node_id)
 
-                prediction = scorer_fn(i_node, j_node)
-                label = i_node.connected_to_id(j_node_id)
+                    prediction = scorer_fn(i_node, j_node)
+                    label = i_node.connected_to_id(j_node_id)
 
-                results[results_key] = (prediction, label)
-                if prediction == label:
-                    correct += 1
+                    if label == True:
+                        total_positive += 1
+                        if prediction == label:
+                            correct_positive += 1
 
-        return correct, explored 
+                    else:
+                        total_negative += 1
+                        if prediction == label:
+                            correct_negative += 1
 
-    num_workers = 10
-    interval_length = len(node_ids) / num_workers
-    idxs = np.cumsum([0] + [interval_length for _ in range(num_workers - 1)])
+            results_queue.put((correct_negative, correct_positive, total_negative, total_positive))
+        except Exception as e:
+            print(e)
+
+    num_intervals = max(1, int(math.sqrt(num_workers)))
+    interval_length = int(len(node_ids) / num_intervals)
+    idxs = np.cumsum([0] + [interval_length for _ in range(num_intervals - 1)])
     idxs = np.append(idxs, len(node_ids))
-
-    evaluation_executor = ThreadPoolExecutor(max_workers=num_workers)
-    result_futures = []
+    ranges = []
     for i in range(len(idxs) - 1):
-        idx_low = idxs[i]
-        idx_high = idxs[i + 1]
-        result_future = evaluation_executor.submit(eval_helper, idx_low, idx_high)
-        result_futures.append(result_future)
+        r_low = idxs[i]
+        r_high = idxs[i + 1]
+        ranges.append((r_low, r_high))
 
-    total_correct = 0
-    total_explored = 0
-    for result_future in result_futures:
-       correct, explored = result_future.result()
-       total_correct += correct
-       total_explored += explored
+    range_pairs = [(r1, r2) for r1, r2 in itertools.product(ranges, ranges)]
+    unique_set = set()
+    new_pairs = []
+    for pair in range_pairs:
+        unique_key = frozenset(pair)
+        if unique_key not in unique_set:
+            new_pairs.append(pair)
+            unique_set.add(unique_key)
 
-    print("Total Correct: {}, Total Explored: {}, Ratio: {}".format(total_correct, total_explored, float(total_correct) / total_explored))
+    range_pairs = new_pairs
+
+    range_idx = 0
+    result_procs = []
+
+    total = 0
+    while range_idx < len(range_pairs):
+        r1, r2 = range_pairs[range_idx]
+        r1_low, r1_high = r1
+        r2_low, r2_high = r2
+
+        result_proc = Process(target=eval_helper, args=(graph, node_ids, r1_low, r1_high, r2_low, r2_high, results_queue))
+        result_proc.start()
+        result_procs.append(result_proc)
+
+        range_idx += 1
+
+    for proc in result_procs:
+        proc.join()
+
+    total_correct_negative = 0
+    total_correct_positive = 0
+    total_negative = 0
+    total_positive = 0
+    while not results_queue.empty():
+        correct_negative, correct_positive, explored_negative, explored_positive = results_queue.get()
+        total_correct_negative += correct_negative
+        total_correct_positive += correct_positive
+        total_negative += explored_negative
+        total_positive += explored_positive
+
+    # total_false_positive = (
+    # true_positive_ratio = float(total_correct_positive) / total_positive
+    # false_positive_ratio = 
+    #
+    # print("Total Correct: {}, Total Explored: {}, Ratio: {}".format(total_correct, total_explored, float(total_correct) / total_explored))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Construct a facebook graph given node and edge inputs')
@@ -203,8 +248,8 @@ if __name__ == "__main__":
 
     graph = construct_network_graph(args.path)
     # common_neighbors_scorer = create_scorer(method_common_neighbors, 1)
-    common_neighbors_scorer = create_scorer(method_common_neighbors_exclude_selves, 1)
-    eval_link_prediction_method(graph, common_neighbors_scorer)
+    common_neighbors_scorer = create_scorer(method_common_neighbors_exclude_selves, 10)
+    eval_link_prediction_method(graph, common_neighbors_scorer, 64)
 
     end = datetime.now()
     print((end - before).total_seconds())
